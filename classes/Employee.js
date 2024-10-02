e2963a0dcabac650bf367cea363b7fdce35ec6e1
apatni123 <aishah.patni@barclays.com>
2024-10-02 17:35:58 +0100

    finished

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,11 +1,17 @@
 class Employee {
     #salary
     #isHired
+    static #allEmployees=[]
+
+
+
     constructor(name,position,salary){
         this.name=name
         this.position=position
         this.#salary=salary
         this.#isHired=true
+
+        Employee.#allEmployees.push(this);
     }
 
     getSalary(){
@@ -28,6 +34,19 @@ class Employee {
         }
     }
 
+    static getEmployees() {
+        console.log(this.#allEmployees)
+        return this.#allEmployees;
+    }
+
+    static getTotalPayroll() {
+        let total = 0; // Initialize total to 0
+        // Loop through each employee in the allEmployees array
+        for (const employee of this.#allEmployees) {
+            total += employee.getSalary(); // Add the employee's salary to the total
+        }
+        return total; // Return the total salary of all employees
+    }
 
     
 }
